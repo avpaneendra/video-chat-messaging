b5a614497b34dc93c809f8869e36872e766867aa
Tauseef Naqvi <tauseef.naqvi786@gmail.com>
2018-08-11 17:49:37 +0530

    change socket host

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const config = {
     'port':5500,
-    'socket_host': 'http://localhost:5000',
+    //'socket_host': 'http://localhost:5000', //for local
+    'socket_host': 'https://socket.ethorbit.com',
 };
 module.exports = config;
